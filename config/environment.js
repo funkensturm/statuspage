0a742a51718f1021d1b70030fdf666b6993edf62
Manuel Wiedenmann <manuel@funkensturm.de>
2016-01-24 16:11:25 +0100

    Rename modulePrefix

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'status',
+    modulePrefix: 'statuspage',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
